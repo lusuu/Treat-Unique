23c4e2706d4d96c095cac23cb31db1395ad4773f
DESKTOP-DOASQLK\lusuu <parapro.kh@gmail.com>
2017-05-27 19:02:08 +0300

    add new version 27052017

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -7,3 +7,20 @@ $(".input-field .wpcf7-form-control").focus(function() {
 $(".input-field .wpcf7-form-control").focusout(function(){
   $(this).closest(".input-field").removeClass('focused');
 });
+
+$('.filter-custom-dropdown .dropbtn').click(function() {
+	$('.filter-dropdown-content').removeClass('hide');
+	$('.filter-dropdown-content').toggleClass('show');
+});
+
+$(document).mouseup(function (e) {
+	var container = $('.filter-custom-dropdown');
+	if (container.has(e.target).length === 0){
+		$('.filter-dropdown-content').removeClass('show');
+		$('.filter-dropdown-content').addClass('hide');
+	}
+});
+
+$('.filter-dropdown-content li').click(function() {
+	$('.filter-dropdown-content').removeClass('show');
+});
